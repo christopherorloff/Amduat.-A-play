d0f91cbab837e4b6ff522469668cdf69be839c20
Spejderman <r.odgaard@gmail.com>
2019-10-04 11:23:05 +0200

    created scene and class for input manager

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    //Notes
+        //Remember to have a variable to invert direction depending on scroll direction
+
+
+    // Detect beginning of input - Event maybe?
+    
+    // Bool - Input or not
+
+    // Direction - Are we receiving input, and is it up or down
+
+    // Input with acceleration (artificial)
+
+    // Input with acceleration (system based)
+}
