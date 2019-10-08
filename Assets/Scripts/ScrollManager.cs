2943096320ee8b7aa6cd50029581e875d1e2e070
Spejderman <r.odgaard@gmail.com>
2019-10-08 20:32:30 +0200

    added more functionality to ScrollManager

--- Assets/Scripts/ScrollManager.cs
+++ Assets/Scripts/ScrollManager.cs
@@ -8,30 +8,48 @@ namespace ScrollManager
     public class Scroll : MonoBehaviour
     {
         //Notes
-        //Remember to have a variable to invert direction depending on scroll direction
+            //Remember to have a variable to invert direction depending on scroll direction
+            //Windows scalar
+            //Try different mice
 
         //Variables for internal use:
         static float lastValue;
         static float lastValueVar;
         static Queue<float> lastValues = new Queue<float>();
-        
+
         // Detect beginning of input - Event maybe?
             // Very hard to do
 
         // Bool - Input or not
         public static bool isScrolling()
         {
-            bool status = false;
-            status = (scrollValue() != 0) ? true : false;
-            return status;
+            bool output = false;
+            output = (scrollValue() != 0) ? true : false;
+            return output;
         }
+        
         // Direction - Are we receiving input, and is it up or down
-
+        public static string scrollDirection()
+        {
+            string output = "None";
+            if (scrollValue() != 0)
+            {
+                if (scrollValue() > 0)
+                {
+                    output = "Up";
+                }else
+                {
+                    output = "Down";
+                }
+            }
+            return output;
+        }
         // Input with acceleration (artificial) //
+            //Time for how long the input goes on
 
         // Input with acceleration (system based) //
 
-        //The basis for all scroll values. Singular instances of zero value filtered
+        //The basis for all scroll values. Singular instances of zero value filtered out
         public static float scrollValue()
         {
             float newValue = Input.mouseScrollDelta.y;
@@ -56,11 +74,12 @@ namespace ScrollManager
             return output;
         }
 
+        // Scroll value that takes the previous n number of values into account
         public static float scrollValueMean(int n)
         {
             float output = 0;
-
             lastValues.Enqueue(scrollValue());
+            
             if (lastValues.Count > n)
             {
                 lastValues.Dequeue();
@@ -69,9 +88,12 @@ namespace ScrollManager
             return output;
         }
 
+
+        // ----------------------------- Helper functions ----------------------------- \\
         private static float GetMeanOfQueue(int n, Queue<float> queue)
         {
             float mean = 0;
+
             foreach (var item in queue)
             {
                 mean += item;
@@ -80,5 +102,4 @@ namespace ScrollManager
             return mean;
         }
     }
-
 }
