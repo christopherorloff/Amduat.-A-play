4c57d7b5b110dc36bbcac3401f4f44d171994f5a
Spejderman <r.odgaard@gmail.com>
2019-10-08 10:48:39 +0200

    initial commit of scroll namespace

---
+++ Assets/Scripts/ScrollManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ScrollManager
+{
+    public class Scroll : MonoBehaviour
+    {
+        //Notes
+        //Remember to have a variable to invert direction depending on scroll direction
+        
+
+        // Detect beginning of input - Event maybe?
+
+        // Bool - Input or not
+        public static bool isScrolling()
+        {
+            bool status = false;
+            status = (Input.mouseScrollDelta.y != 0)?true:false;
+            return status;
+        }
+        // Direction - Are we receiving input, and is it up or down
+
+        // Input with acceleration (artificial)
+
+        // Input with acceleration (system based)
+    }
+
+}
