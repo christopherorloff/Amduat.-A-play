c55e7042004a938f6b7860e1dc9dbf3170c77740
Jacob Amstrup <jacobhvidamstrup@gmail.com>
2019-10-11 13:33:16 +0200

    Singleton SoundManager object

---
+++ Assets/SoundManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+using ScrollManager;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance;
+
+    private void Awake()
+    {
+        //Checking that only one instance exists
+        if (Instance == null) {
+            Instance = FindObjectOfType<SoundManager>(); 
+
+            if(Instance == null) {
+                Instance = this;
+            } else {
+                Destroy(this);
+            }
+        } 
+    }
+}
