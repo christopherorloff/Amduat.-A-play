e7502b9c332906d7d6f06965bfb75e087bad636b
TusindårsFalken <christopher@alloff.dk>
2019-10-13 16:26:27 +0200

    movement script

---
+++ Assets/Christopher/Scripts/RotateScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateScript : MonoBehaviour
+{
+    [Range (-10,10)]
+    public float turnSpeed;
+
+    [Range (-10,10)]
+    public float intensity;
+
+    public GameObject wheel;
+    public bool wheelControlled;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(wheelControlled)
+        {
+            transform.Rotate(new Vector3(0, 0, /*wheel.input **/ intensity));
+        }
+        else
+        {
+            transform.Rotate(new Vector3(0, 0, turnSpeed));
+        }
+        
+    }
+}
