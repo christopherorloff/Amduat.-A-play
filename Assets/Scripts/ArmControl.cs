d7548e7167a0baca5e3a7cbf063f0bb55de1a000
TusindårsFalken <christopher@alloff.dk>
2019-10-22 14:08:07 +0200

    Goddess Snap

--- Assets/Scripts/ArmControl.cs
+++ Assets/Scripts/ArmControl.cs
@@ -9,10 +9,11 @@ public class ArmControl : MonoBehaviour
     //private
     float input;
     Transform root;
-    public GameObject snake;
-    ThrowScript throwScript;
-
     
+    ThrowScript throwScript;
+    float snapBack;
+
+
     float maxAngle = 10;
     float minAngle = -6;
 
@@ -20,8 +21,9 @@ public class ArmControl : MonoBehaviour
     public float scalar = 1;
     public float deltaThreshold = 0.3f;
     public float drag = 0.9f;
-    private float velocity = 0;
-    
+    public float snapBackSpeed = 0;
+    public GameObject snake;
+
     void Start()
     {
         root = FindComponentInChildWithTag(this.gameObject,"Root");  
@@ -38,7 +40,8 @@ public class ArmControl : MonoBehaviour
 
     private void TakeInput()
     {
-        input = Scroll.scrollValueAccelerated() * scalar * drag;
+        //input = Scroll.scrollValueAccelerated() * scalar * drag;
+        input = Scroll.scrollValue() * scalar * drag;
     }
 
     private void ProcessInput()
@@ -59,11 +62,10 @@ public class ArmControl : MonoBehaviour
         {
             root.rotation = Quaternion.Euler(0, 0, minAngle + 360);
         }
-
-        if(input == 0)
+        else
         {
-
-            root.rotation = Quaternion.Euler(0, 0, Mathf.SmoothDamp(transform.position.z, 0f, ref velocity ,2.0f));
+            snapBack = Mathf.Lerp(z, 0, snapBackSpeed * Time.deltaTime);
+            root.rotation = Quaternion.Euler(0, 0, snapBack + 360);
         }
     }
 
