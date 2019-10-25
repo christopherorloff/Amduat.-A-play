e0d1daed5408e57b8722ad2342d7fe185d2bd92a
Jacob Amstrup <jacobhvidamstrup@gmail.com>
2019-10-25 14:40:28 +0200

    Lavet prototype på time 3. Hedder Hour3_Prototype

---
+++ Assets/Scripts/Limb.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Limb : MonoBehaviour
+{
+    //TARGET VARIABLES
+    public Transform target;
+    public float smoothTime = 1f;
+    private Vector3 velocity = Vector3.zero;
+
+    public bool isMoving;
+    public bool isActive;
+
+    private void Update()
+    {
+        if (isMoving) {
+            transform.position = Vector3.SmoothDamp(transform.position, target.position, ref velocity, smoothTime);
+        }
+
+        if (isActive) { 
+            
+        }
+    }
+}
---
+++ Assets/Scripts/LimbMovement.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ScrollManager;
+
+public class LimbMovement : MonoBehaviour
+{
+    //SCROLL VARIABLES
+    bool lastDirectionUp = true;
+
+    //LIMB VARIABLES
+    public GameObject[] limbs;
+    public int counter;
+
+    //ROTATION VARIABLES
+    public Vector3[] rotationTargets;
+    public float speed = 0.3F;
+    private Vector3 velocity = Vector3.zero;
+
+    private void Start()
+    {
+        //Setting first limb from limbs array to be the active limb
+        limbs[counter].GetComponent<Limb>().isActive = true;
+    }
+
+    void Update()
+    {
+        CheckIfMoving();
+
+        //Rotate camera
+        float step = speed * Time.deltaTime;
+
+        if(counter >= 1) {
+            Quaternion rotation = Quaternion.Euler(rotationTargets[counter - 1]);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, step);
+        }
+    }
+
+    void Move()
+    {
+        counter++;
+        if(counter-1 < limbs.Length) {
+            //Setting the correct limb to move
+            limbs[counter - 1].GetComponent<Limb>().isMoving = true;
+            limbs[counter].GetComponent<Limb>().isActive = true;
+        }
+    }
+
+    //Checking if last direction is opposite of the previous one - if it is, then a limb will move
+    void CheckIfMoving() {
+        if (lastDirectionUp) {
+            if (Scroll.scrollValue() < -0.05f) {
+                //Setting current limb from limbs array to be not active limb
+                limbs[counter].GetComponent<Limb>().isActive = false;
+
+                Move();
+                lastDirectionUp = false;
+            }
+        }
+        else if (!lastDirectionUp) {
+            if (Scroll.scrollValue() > 0.05f) {
+                //Setting current limb from limbs array to be not active limb
+                limbs[counter].GetComponent<Limb>().isActive = false;
+
+                Move();
+                lastDirectionUp = true;
+            }
+        }
+    }
+}
