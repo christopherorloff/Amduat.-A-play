aa035236bf282840ba0a4291ecb4193281bef43a
Nikolaj Christensen <schildt.nikolaj@gmail.com>
2019-10-27 15:45:47 +0100

    Scene 4 prototype-logik nogenlunde på plads

---
+++ Assets/MoveBoat.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBoat : MonoBehaviour
+{
+
+    public TowBoatLogic TowScript;
+    float moveSpeed = 1f;
+
+    void Start()
+    {
+        TowScript = FindObjectOfType<TowBoatLogic>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (TowScript.pulling)
+        {
+            transform.position += new Vector3(moveSpeed * Time.deltaTime, 0, 0);
+
+        }
+    }
+}
