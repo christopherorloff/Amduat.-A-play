032e9c825743cd0dfcf7096ead14b541090f88d1
TusindårsFalken <christopher@alloff.dk>
2019-10-29 10:11:35 +0100

    Folders organizing

---
+++ Assets/Scripts/Shake.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shake : MonoBehaviour
+{
+    public float magnitude;
+
+    void Update()
+    {
+        float x = Random.Range(-1, 1) * magnitude;
+        float y = Random.Range(-1, 1) * magnitude;
+        transform.localPosition = new Vector3(x, y, transform.position.z);
+    }
+}
