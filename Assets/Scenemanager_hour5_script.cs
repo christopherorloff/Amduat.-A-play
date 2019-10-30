d7bbe14ab98f1f220b2af9ff3407b5579abdd76e
Nikolaj Christensen <schildt.nikolaj@gmail.com>
2019-10-30 09:11:37 +0100

    Scene 5 prototype work in progress

---
+++ Assets/Scenemanager_hour5_script.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scenemanager_hour5_script : MonoBehaviour
+{
+
+    public GameObject Boat;
+    public CheckCollisonBoat_Script TriggerScript;
+    int numberOfSnakes;
+    float boatMoveSpeedX;
+
+    void Start()
+    {
+        boatMoveSpeedX = 0.7f;
+    }
+
+    void Update()
+    {
+
+        Boat.transform.position += new Vector3(boatMoveSpeedX * Time.deltaTime, 0, 0);
+
+
+        if (TriggerScript.numberOfSnakes > 3)
+        {
+            boatMoveSpeedX = 0.5f;
+        }
+        if (TriggerScript.numberOfSnakes > 7)
+        {
+            boatMoveSpeedX = 0.4f;
+        }
+        if (TriggerScript.numberOfSnakes > 10)
+        {
+            boatMoveSpeedX = 0.2f;
+        }
+        if (TriggerScript.numberOfSnakes > 14)
+        {
+            boatMoveSpeedX = 0.1f;
+        }
+        if (TriggerScript.numberOfSnakes > 18)
+        {
+            boatMoveSpeedX = 0.0f;
+        }
+    }
+
+
+
+    
+}
---
+++ Assets/SnakeMoveTowardsBoat_script.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeMoveTowardsBoat_script : MonoBehaviour
+{
+
+    public GameObject Boat;
+    float speed;
+
+    void Start()
+    {
+        speed = Random.Range(0.3f, 0.8f);
+        Boat = GameObject.FindGameObjectWithTag("Boat");
+    }
+
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, Boat.transform.position, speed * Time.deltaTime);
+    }
+}
