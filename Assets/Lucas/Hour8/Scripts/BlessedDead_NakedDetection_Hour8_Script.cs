4c8b5be2d6ae1af07bb24ba637a56ca57fcb2593
Lucas A. V. Møller <luje@itu.dk>
2019-10-31 13:50:13 +0100

    lucas' folder

---
+++ Assets/Lucas/Hour8/Scripts/BlessedDead_NakedDetection_Hour8_Script.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlessedDead_NakedDetection_Hour8_Script : MonoBehaviour
+{
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "BlessedDead")
+        {
+            collision.GetComponent<BlessedDead_Transformation_Hour8_Script>().isNaked = true;
+        }
+    }
+}
