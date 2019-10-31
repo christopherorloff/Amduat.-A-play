b658cba015c78ca0786f433fd82e400126043691
Nikolaj Christensen <schildt.nikolaj@gmail.com>
2019-10-31 12:23:26 +0100

    Hour 5 prototype logic ready for review

--- Assets/Scenemanager_hour5_script.cs
+++ Assets/Scenemanager_hour5_script.cs
@@ -1,17 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using ScrollManager;
 
 public class Scenemanager_hour5_script : MonoBehaviour
 {
 
     public GameObject Boat;
+    public GameObject wing1;
+
+    Animation anim;
+    public AnimationClip wingUp;
+    public AnimationClip wingDown;
+
     public CheckCollisonBoat_Script TriggerScript;
+
     int numberOfSnakes;
     float boatMoveSpeedX;
+    float animSpeed = 0.5f;
+
+    bool readyToFlap = false;
+    public bool pushSnakesAway = false;
 
     void Start()
     {
+        anim = wing1.GetComponent<Animation>();
         boatMoveSpeedX = 0.7f;
     }
 
@@ -19,31 +32,85 @@ public class Scenemanager_hour5_script : MonoBehaviour
     {
 
         Boat.transform.position += new Vector3(boatMoveSpeedX * Time.deltaTime, 0, 0);
+        print(boatMoveSpeedX);
 
-
-        if (TriggerScript.numberOfSnakes > 3)
+        if (TriggerScript.numberOfSnakes >= 2)
         {
             boatMoveSpeedX = 0.5f;
         }
-        if (TriggerScript.numberOfSnakes > 7)
+        if (TriggerScript.numberOfSnakes > 5)
         {
             boatMoveSpeedX = 0.4f;
         }
-        if (TriggerScript.numberOfSnakes > 10)
+        if (TriggerScript.numberOfSnakes > 7)
         {
             boatMoveSpeedX = 0.2f;
         }
-        if (TriggerScript.numberOfSnakes > 14)
+        if (TriggerScript.numberOfSnakes > 10)
         {
             boatMoveSpeedX = 0.1f;
         }
-        if (TriggerScript.numberOfSnakes > 18)
+        if (TriggerScript.numberOfSnakes > 15)
         {
             boatMoveSpeedX = 0.0f;
         }
+        else if (TriggerScript.numberOfSnakes < 2)
+        {
+            boatMoveSpeedX = 0.7f;
+
+        }
+        
+
+
+        if (Scroll.scrollValueAccelerated() < 0 && !readyToFlap)
+        {
+            anim.clip = wingUp;
+            anim["wingUp"].speed = animSpeed;
+            anim.Play();
+        }
+
+        if (Scroll.scrollValueAccelerated() == 0 && !readyToFlap)
+        {
+            anim.clip = wingUp;
+            anim["wingUp"].speed = 0;
+            anim.Play();
+        }
+
+        if (anim["wingUp"].normalizedTime >= 0.99f && !readyToFlap)
+        {
+            readyToFlap = true;
+        }
+
+
+        if (Scroll.scrollValueAccelerated() > 0 && readyToFlap)
+        {
+            anim.clip = wingDown;
+            anim["wingDown"].speed = animSpeed;
+            anim.Play();
+        }
+
+        if (anim["wingDown"].normalizedTime >= 0.99f)
+        {
+            StartCoroutine(TurnBoolOff());
+        }
+
+        
+
+
+    }
+
+
+    public IEnumerator TurnBoolOff()
+    {
+        pushSnakesAway = true;
+        yield return new WaitForSeconds(1f);
+        pushSnakesAway = false;
+        readyToFlap = false;
+
+
     }
 
 
 
-    
+
 }
--- Assets/SnakeMoveTowardsBoat_script.cs
+++ Assets/SnakeMoveTowardsBoat_script.cs
@@ -4,18 +4,32 @@ using UnityEngine;
 
 public class SnakeMoveTowardsBoat_script : MonoBehaviour
 {
+    public Scenemanager_hour5_script SceneManager;
 
     public GameObject Boat;
     float speed;
+    float awaySpeed = 2f;
+    float distance = 1f;
 
     void Start()
     {
         speed = Random.Range(0.3f, 0.8f);
         Boat = GameObject.FindGameObjectWithTag("Boat");
+        SceneManager = FindObjectOfType<Scenemanager_hour5_script>();
     }
 
     void Update()
     {
         transform.position = Vector2.MoveTowards(transform.position, Boat.transform.position, speed * Time.deltaTime);
+
+        if (SceneManager.pushSnakesAway)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, Boat.transform.position, -1 * awaySpeed * Time.deltaTime);
+        }
+        if (Vector3.Distance(Boat.transform.position, transform.position) <=1)
+        {
+            transform.position = (transform.position - Boat.transform.position).normalized * distance + Boat.transform.position;
+
+        }
     }
 }
