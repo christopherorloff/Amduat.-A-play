d6f1689ae6fbbc487219264d05d83dceeaea5718
Spejderman <r.odgaard@gmail.com>
2019-11-01 13:25:08 +0100

    added public functions to gamemanager

---
+++ Assets/Sandboxes/Rasmus/BoidController_Script.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoidController_Script : MonoBehaviour
+{
+    BoidIndividual_Script[] flock;
+
+    void Start()
+    {
+        flock = GetComponentsInChildren<BoidIndividual_Script>();
+
+        for (int i = 0; i < flock.Length; i++)
+        {
+            //Get local boids
+            //
+        }
+    }
+
+}
