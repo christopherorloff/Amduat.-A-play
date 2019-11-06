c2cee7db2487b99d97af6785d406389568ff4847
Spejderman <r.odgaard@gmail.com>
2019-11-07 00:27:52 +0100

    added beautiful ms paint colors to growing trees

--- Assets/Sandboxes/Rasmus/BoidIndividual_Script.cs
+++ Assets/Sandboxes/Rasmus/BoidIndividual_Script.cs
@@ -4,33 +4,19 @@ using UnityEngine;
 
 public class BoidIndividual_Script : MonoBehaviour
 {
-    private List <Collider2D> localFlockColliders;
-    private List <BoidIndividual_Script> localFlock;
+    private List<Collider2D> localFlockColliders;
     private ContactFilter2D contactFilter2D;
-    private Collider2D collider2D;
-
 
     void Start()
     {
-        collider2D = GetComponent<Collider2D>();
-        localFlock = new List<BoidIndividual_Script>();
         localFlockColliders = new List<Collider2D>();
         contactFilter2D.useTriggers = true;
     }
 
-    public void GetLocalFlock(float radius)
+    public List<Collider2D> GetLocalFlock(float radius)
     {
-        localFlock.Clear();
-        Physics2D.OverlapCircle(this.transform.position, radius,contactFilter2D,localFlockColliders);
-        print(this.name);
-        foreach (var item in localFlockColliders)
-        {
-            if (item.name == this.name)
-            {
-                continue;
-            }
-            print("-- " + item.name);
-        }
-
+        localFlockColliders.Clear();
+        Physics2D.OverlapCircle(this.transform.position, radius, contactFilter2D, localFlockColliders);
+        return localFlockColliders;
     }
 }
