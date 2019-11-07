10841ba400fbd79bb047b08db030b35fe66fdd62
Nikolaj Christensen <schildt.nikolaj@gmail.com>
2019-11-07 15:49:31 +0100

    flere particle effects

---
+++ Assets/Particles_moveTowards_object.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Particles_moveTowards_object : MonoBehaviour
+{
+
+    public ParticleSystem p;
+    public ParticleSystem.Particle[] particles;
+    public Transform Target;
+    public float affectDistance;
+    float sqrDist;
+    Transform thisTransform;
+
+
+    void Start()
+    {
+        p = GetComponent<ParticleSystem>();
+        sqrDist = affectDistance * affectDistance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    
+        particles = new ParticleSystem.Particle[p.particleCount];
+        p.GetParticles(particles);
+        for (int i = 0; i < particles.GetUpperBound(0); i++)
+        {
+            float ForceToAdd = (particles[i].startLifetime - particles[i].remainingLifetime) * (20 * Vector3.Distance(Target.position, particles[i].position));
+            particles[i].position = Vector3.Lerp(particles[i].position, Target.position, Time.deltaTime / 2.0f);
+
+
+        }
+        p.SetParticles(particles, particles.Length);
+    }
+}
