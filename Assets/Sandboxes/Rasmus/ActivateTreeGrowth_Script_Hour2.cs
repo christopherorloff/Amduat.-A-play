34b331018ed744d010bb335d1ff8aff8c2ef3542
Spejderman <r.odgaard@gmail.com>
2019-11-07 15:52:43 +0100

    basic scene2 prototype

---
+++ Assets/Sandboxes/Rasmus/ActivateTreeGrowth_Script_Hour2.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateTreeGrowth_Script_Hour2 : MonoBehaviour
+{
+    PlantGrowthController_Script tree;
+    void Start()
+    {
+        tree = GetComponent<PlantGrowthController_Script>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        tree.enabled = true;
+    }
+}
---
+++ Assets/Sandboxes/Rasmus/MoveBoat_Script_Hour2.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ScrollManager;
+
+public class MoveBoat_Script_Hour2 : MonoBehaviour
+{
+    public float speed = 3;
+    public float drag = 0.99f;
+    void Update()
+    {
+        if (Scroll.scrollValueAccelerated(drag) > 0)
+        {
+            print("Input: " + Scroll.scrollValueAccelerated(drag));
+            transform.position += new Vector3(Scroll.scrollValueAccelerated(drag) * Time.deltaTime * speed, 0, 0);
+        }
+    }
+}
