3555814a5fdf991b411c1956c79ca51cf941cc95
TusindårsFalken <christopher@alloff.dk>
2019-11-25 19:42:49 +0100

    Boat Animation hour1
    Have created a script which animates the boat in hour 1. I have also corrected the Y position of the blessed dead, so it fits the new sprite

---
+++ Assets/Boat_Animator_Script.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boat_Animator_Script : MonoBehaviour
+{
+    public Sprite[] boatSprites;
+    public SpriteRenderer boatSpriteRenderer;
+    public float animationTime;
+
+    private IEnumerator coroutine;
+
+    private int numberOfAnim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        numberOfAnim = boatSprites.Length-1;
+        coroutine = WaitAndPrint(animationTime);
+        StartCoroutine(coroutine);
+    }
+
+    private IEnumerator WaitAndPrint(float waitTime)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(waitTime);
+            boatSpriteRenderer.sprite = boatSprites[numberOfAnim];
+            if(numberOfAnim >= boatSprites.Length-1)
+            {
+                numberOfAnim = -1;
+                
+            }
+            numberOfAnim++;
+        }
+    }
+    // Update is called once per frame
+}
