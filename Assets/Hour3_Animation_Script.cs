ab86bc9a5cc69e68ee7a36bdb35d9d7cee6af938
TusindårsFalken <christopher@alloff.dk>
2019-11-25 22:56:54 +0100

    Hour3 animation

--- Assets/Hour3_Animation_Script.cs
+++ Assets/Hour3_Animation_Script.cs
@@ -6,19 +6,20 @@ public class Hour3_Animation_Script : MonoBehaviour
 {
     // Start is called before the first frame update
     public Sprite[] sprites;
-    public SpriteRenderer spriteRenderer;
-    public SpriteRenderer reflectionSpriteRenderer;
+    public SpriteRenderer spriteRenderer1;
+    public SpriteRenderer spriteRenderer2;
     public float animationTime;
 
     private IEnumerator coroutine;
 
     private float t = 0f;
     private int numberOfAnim;
-    private float min1 = -2f;
-    private float max1 = 2f;
+    private float min1 = 0f;
+    private float max1 = 1f;
     // Start is called before the first frame update
     void Start()
     {
+        spriteRenderer1.sprite= sprites[0];
         coroutine = WaitAndPrint(animationTime);
         StartCoroutine(coroutine);
     }
@@ -31,7 +32,27 @@ public class Hour3_Animation_Script : MonoBehaviour
         {
             yield return new WaitForSeconds(waitTime);
             
-            spriteRenderer.sprite = sprites[numberOfAnim];
+            spriteRenderer2.sprite = spriteRenderer1.sprite;
+            spriteRenderer1.sprite = sprites[numberOfAnim];
+
+            spriteRenderer1.color  = new Color(1,1,1,Mathf.Lerp(min1,max1,t));
+
+            if (t < 1f)
+            { 
+            t += Time.deltaTime/animationTime;
+            //Debug.Log(t);
+            }
+
+            if (t >= 1.0f)
+            {
+                float temp1 = max1;
+                max1 = min1;
+                min1 = temp1;
+
+
+                t = 0.0f;
+            }
+
             if(numberOfAnim >= sprites.Length-1)
             {
                 numberOfAnim = -1;
