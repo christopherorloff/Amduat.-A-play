766d74f74568d8679458fe3ee5dbc287ea6500b6
Spejderman <r.odgaard@gmail.com>
2019-12-03 10:07:39 +0100

    added unfinished version of sandbox hour8. Some additional cleanup

---
+++ Assets/Sandboxes/Rasmus/Hour8 TImeline prototype/BlessedDeadBehaviour_Script_Hour8.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlessedDeadBehaviour_Script_Hour8 : MonoBehaviour
+{
+    //Behaviour
+    private Vector3 velocity;
+
+    //Screen stats
+
+    Vector2 screenSizeMax = new Vector2(Screen.width, Screen.height);
+    Vector2 screenSizeMin = Vector2.zero;
+
+    private Vector2 screenBoundariesMax;
+    private Vector2 screenBoundariesMin;
+    private Vector2 screenWorldSize;
+
+    //Interface --> move to parent object
+    public float clampedYPosition = 1 / 3;
+    public float speed = 1;
+
+    void Start()
+    {
+
+        //Setup for constant variables
+        screenBoundariesMax = (Vector2)Camera.main.ScreenToWorldPoint((Vector3)screenSizeMax);
+        screenBoundariesMin = (Vector2)Camera.main.ScreenToWorldPoint((Vector3)screenSizeMin);
+        screenWorldSize.x = screenBoundariesMax.x - screenBoundariesMin.x;
+        screenWorldSize.y = screenBoundariesMax.y - screenBoundariesMin.y;
+        Init();
+    }
+
+    void Update()
+    {
+        Move();
+        CheckForBoundaries();
+    }
+
+    private void Move()
+    {
+        transform.position += velocity * speed * Time.deltaTime;
+    }
+
+    private void Move(Vector3 input)
+    {
+        transform.position += input * speed * Time.deltaTime;
+    }
+
+    private void Init()
+    {
+        velocity = Vector3.left;
+        transform.position = new Vector3(screenBoundariesMax.x + UnityEngine.Random.Range(0.0f, 2.0f), GetRandomYPosition(), 0);
+    }
+
+    private void CheckForBoundaries()
+    {
+
+        if (transform.position.x < screenBoundariesMin.x)
+        {
+            transform.position = new Vector3(screenBoundariesMax.x, GetRandomYPosition(), 0);
+        }
+    }
+
+
+    private float GetRandomYPosition()
+    {
+        float clamp = screenWorldSize.y / 2 * clampedYPosition;
+        float y = UnityEngine.Random.Range(-clamp, clamp);
+        return y;
+    }
+}
---
+++ Assets/Sandboxes/Rasmus/Hour8 TImeline prototype/BlessedDeadRepeller_Script_Hour8.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class BlessedDeadRepeller_Script_Hour8 : MonoBehaviour
+{
+    public float overlapRadius = 1;
+    private ContactFilter2D contactFilter2D;
+    private List<Collider2D> colliders;
+
+    void Start()
+    {
+        colliders = new List<Collider2D>();
+
+    }
+
+    void Update()
+    {
+        colliders.Clear();
+        Physics2D.OverlapCircle((Vector2)transform.position, overlapRadius, contactFilter2D, colliders);
+        Check
+    }
+}
