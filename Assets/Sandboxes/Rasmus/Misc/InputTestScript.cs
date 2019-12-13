f69aa3158bc6e2f161e2dd9c13382026814a54c2
Spejderman <r.odgaard@gmail.com>
2019-12-13 14:55:52 +0100

    fixed inputtest bug

--- Assets/Sandboxes/Rasmus/Misc/InputTestScript.cs
+++ Assets/Sandboxes/Rasmus/Misc/InputTestScript.cs
@@ -8,22 +8,12 @@ public class InputTestScript : MonoBehaviour
     // Start is called before the first frame update
     void OnEnable()
     {
-<<<<<<< HEAD
-
-=======
-        Scroll.OnScrollEnter += OnEnter;
-        Scroll.OnScrollExit += OnExit;
->>>>>>> 9ca83829bde308d5d83ae68ef926657928314474
     }
 
     // Update is called once per frame
     void Update()
     {
-<<<<<<< HEAD
         print("InputValue: " + Scroll.scrollValue());
-=======
-       // print("InputValue: " + Scroll.scrollValue());
->>>>>>> 9ca83829bde308d5d83ae68ef926657928314474
     }
 
     void OnEnter() { print("OnEnter"); }
