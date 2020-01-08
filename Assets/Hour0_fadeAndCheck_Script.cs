972af4c82d17c96e5c28a70005218f4c75152e9f
TusindårsFalken <christopher@alloff.dk>
2020-01-08 15:13:37 +0100

    Hour 0 almost done - needs change scene and load banks etc

--- Assets/Hour0_fadeAndCheck_Script.cs
+++ Assets/Hour0_fadeAndCheck_Script.cs
@@ -6,18 +6,22 @@ using UnityEngine.UI;
 
 public class Hour0_fadeAndCheck_Script : Timeline_BaseClass
 {
-    public GameObject Controlls;
+    public GameObject Controlls1;
+    public GameObject Controlls2;
     public Text Text1;
     public Text Text2;
     public SpriteRenderer FadeToBlack;
     public float InputScalar = 1;
-
+    public float animationTime2;
     public float scrollScalar = 1;
     public float CheckTime = 0.5f;
     public bool Inverted;
+    public bool locked;
 
+    private IEnumerator coroutine2;
+    private bool firstend;
     private bool ended;
-    private float fadeScalar = 10f;
+    private float fadeScalar = 15f;
     private  Color textColor;
     private float fadeCount1;
     private float fadeCount2;
@@ -37,6 +41,8 @@ public class Hour0_fadeAndCheck_Script : Timeline_BaseClass
         FadeToBlack.color = new Color(FadeToBlack.color.r,FadeToBlack.color.g,FadeToBlack.color.b,0);
         Checker = false;
         Checked = false;
+        Controlls1.SetActive(true);
+        Controlls2.SetActive(false);
         HandleKeys();
     }
 
@@ -44,8 +50,10 @@ public class Hour0_fadeAndCheck_Script : Timeline_BaseClass
     void Update()
     {
         input = Scroll.scrollValueAccelerated();
-        ConvertInput(input);
-        
+        if(!locked)
+        {
+            ConvertInput(input);
+        }
     }
 
     private void ConvertInput(float inputCheck)
@@ -80,51 +88,121 @@ public class Hour0_fadeAndCheck_Script : Timeline_BaseClass
                 Checked = true;
             }
         }
-        else if(Inverted && input < 0 && !ended)
+        else if(Inverted && input < 0 && !firstend && !ended)
         {
-            speed = Scroll.scrollValueAccelerated(0.99999f) * InputScalar * Time.deltaTime;
+            speed = Scroll.scrollValueAccelerated(0.99999f) * InputScalar*0.7f * Time.deltaTime;
             speed = Mathf.Clamp(speed, -0.006f, 0);
             speed = Mathf.Abs(speed);
             Timeline += speed;
             Timeline = Mathf.Clamp(Timeline, 0, 1);
-            Debug.Log("Timeline: " +Timeline);
+            Debug.Log("Timeline1: " +Timeline);
             FadeScroll(speed);
         }
-        else if (!Inverted && input > 0 && !ended)
+        else if(Inverted && input > 0 && !ended && firstend)
         {
             speed = Scroll.scrollValueAccelerated(0.99999f) * InputScalar * Time.deltaTime;
             speed = Mathf.Clamp(speed, 0, 0.0006f);
             Timeline += speed;
             Timeline = Mathf.Clamp(Timeline, 0, 1);
-            Debug.Log("Timeline: " +Timeline);
+            Debug.Log("Timeline2: " +Timeline);
+            FadeScroll(speed);
+        }
+        else if (!Inverted && input > 0 && !firstend && !ended)
+        {
+            speed = Scroll.scrollValueAccelerated(0.99999f) * InputScalar * Time.deltaTime;
+            speed = Mathf.Clamp(speed, 0, 0.0006f);
+            Timeline += speed;
+            Timeline = Mathf.Clamp(Timeline, 0, 1);
+            Debug.Log("Timeline3: " +Timeline);
+            FadeScroll(speed);
+        }
+        else if (!Inverted && input < 0 && !ended && firstend)
+        {
+            speed = Scroll.scrollValueAccelerated(0.99999f) * InputScalar*0.7f * Time.deltaTime;
+            speed = Mathf.Clamp(speed, -0.006f, 0);
+            speed = Mathf.Abs(speed);
+            Timeline += speed;
+            Timeline = Mathf.Clamp(Timeline, 0, 1);
+            Debug.Log("Timeline4: " +Timeline);
             FadeScroll(speed);
         }
     }
 
     private void FadeScroll(float scrollValue)
     {
-        Controlls.transform.position += new Vector3(0,-(scrollValue * scrollScalar),0);
+        if(!firstend)
+        {
+            Controlls1.transform.position += new Vector3(0,-(scrollValue * scrollScalar),0);
+        }
+        else if (firstend)
+        {
+            Controlls2.transform.position += new Vector3(0,(scrollValue * scrollScalar),0);
+        }
+        
 
-        if(Timeline > 0.11f && Text1.color.a < 1.0f)
-        {
-            fadeCount1 += scrollValue*fadeScalar;
-            Text1.color = new Color(Text1.color.r,Text1.color.g,Text1.color.b,fadeCount1);
-        }
-        if(Timeline > 0.22f && Text2.color.a < 1.0f)
-        {
-            fadeCount2 += scrollValue*fadeScalar;
-            Text2.color = new Color(Text2.color.r,Text2.color.g,Text2.color.b,fadeCount2);
-        }
-        if(Timeline > 0.35f && FadeToBlack.color.a < 1.0f)
+        if(Timeline > 0.18f && FadeToBlack.color.a < 1.0f && !firstend)
         {
             fadeCount3 += scrollValue*fadeScalar;
             FadeToBlack.color = new Color(FadeToBlack.color.r,FadeToBlack.color.g,FadeToBlack.color.b,fadeCount3);
         }
-        else if (FadeToBlack.color.a >= 1.0f)
+        else if (FadeToBlack.color.a >= 1.0f && !firstend)
+        {
+            firstend = true;
+            coroutine2 = WaitAndPrint2(animationTime2);
+            StartCoroutine(coroutine2);
+        }
+        else if(Timeline > 0.75f && FadeToBlack.color.a < 1.0f && firstend)
+        {
+            fadeCount3 += scrollValue*fadeScalar;
+            FadeToBlack.color = new Color(FadeToBlack.color.r,FadeToBlack.color.g,FadeToBlack.color.b,fadeCount3*1.2f);
+        }
+        else if (Timeline > 0.88f && FadeToBlack.color.a >= 1.0f && firstend)
         {
             EndScene();
         }
         
+
+        if(Timeline > 0.50f && Text2.color.a < 1.0f)
+        {
+            fadeCount2 += scrollValue*fadeScalar;
+            Text2.color = new Color(Text2.color.r,Text2.color.g,Text2.color.b,fadeCount2);
+            if(Text2.color.a >= 0.5f)
+            {
+                Controlls2.SetActive(false);
+            }
+        }
+         if(Timeline > 0.5f && Text1.color.a < 1.0f)
+        {
+            fadeCount1 += scrollValue*fadeScalar;
+            Text1.color = new Color(Text1.color.r,Text1.color.g,Text1.color.b,fadeCount1);
+        }
+        
+        if(Timeline > 0.65f && Text2.color.a >= 1.0f)
+        {
+            Text1.transform.position += new Vector3(0,(scrollValue * scrollScalar),0);
+            Text2.transform.position += new Vector3(0,(scrollValue * scrollScalar),0);
+        }
+
+        
+
+
+        
+    }
+
+    private IEnumerator WaitAndPrint2(float waitTime)
+    {
+        
+            yield return new WaitForSeconds(waitTime);
+            Controlls1.SetActive(false);
+            Controlls2.SetActive(true);
+            FadeToBlack.color = new Color(FadeToBlack.color.r, FadeToBlack.color.g, FadeToBlack.color.b, 1);
+            while (FadeToBlack.color.a > 0.0f)
+            {
+                FadeToBlack.color = new Color(FadeToBlack.color.r, FadeToBlack.color.g, FadeToBlack.color.b, FadeToBlack.color.a - (Time.deltaTime / 2));
+                yield return null;
+            }
+            firstend = true;
+            fadeCount3 = 0;
     }
 
     private void EndScene()
