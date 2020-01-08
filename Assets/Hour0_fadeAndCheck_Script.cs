ac97f791c19a4f77e7af3236ca5e3d7b6240a0e5
TusindårsFalken <christopher@alloff.dk>
2020-01-08 13:13:05 +0100

    Hour 0 function done

--- Assets/Hour0_fadeAndCheck_Script.cs
+++ Assets/Hour0_fadeAndCheck_Script.cs
@@ -6,17 +6,22 @@ using UnityEngine.UI;
 
 public class Hour0_fadeAndCheck_Script : Timeline_BaseClass
 {
-    public GameObject ImageToFade1;
-    public GameObject ImageToFade2;
-    public GameObject TextToFade;
-    public Image FadeToBlack;
+    public GameObject Controlls;
+    public Text Text1;
+    public Text Text2;
+    public SpriteRenderer FadeToBlack;
     public float InputScalar = 1;
 
     public float scrollScalar = 1;
     public float CheckTime = 0.5f;
     public bool Inverted;
 
-    
+    private bool ended;
+    private float fadeScalar = 10f;
+    private  Color textColor;
+    private float fadeCount1;
+    private float fadeCount2;
+    private float fadeCount3;
     private float inputFixed;
     private float speed;
     private float input;
@@ -27,9 +32,11 @@ public class Hour0_fadeAndCheck_Script : Timeline_BaseClass
     // Start is called before the first frame update
     void Start()
     {
+        Text1.color = new Color(Text1.color.r,Text1.color.g,Text1.color.b,0);
+        Text2.color = new Color(Text2.color.r,Text2.color.g,Text2.color.b,0);
+        FadeToBlack.color = new Color(FadeToBlack.color.r,FadeToBlack.color.g,FadeToBlack.color.b,0);
         Checker = false;
         Checked = false;
-        AddTimelineEvent(0.8f, fade);
         HandleKeys();
     }
 
@@ -73,7 +80,7 @@ public class Hour0_fadeAndCheck_Script : Timeline_BaseClass
                 Checked = true;
             }
         }
-        else if(Inverted && input < 0)
+        else if(Inverted && input < 0 && !ended)
         {
             speed = Scroll.scrollValueAccelerated(0.99999f) * InputScalar * Time.deltaTime;
             speed = Mathf.Clamp(speed, -0.006f, 0);
@@ -83,7 +90,7 @@ public class Hour0_fadeAndCheck_Script : Timeline_BaseClass
             Debug.Log("Timeline: " +Timeline);
             FadeScroll(speed);
         }
-        else if (!Inverted && input > 0)
+        else if (!Inverted && input > 0 && !ended)
         {
             speed = Scroll.scrollValueAccelerated(0.99999f) * InputScalar * Time.deltaTime;
             speed = Mathf.Clamp(speed, 0, 0.0006f);
@@ -96,13 +103,33 @@ public class Hour0_fadeAndCheck_Script : Timeline_BaseClass
 
     private void FadeScroll(float scrollValue)
     {
-        ImageToFade2.transform.position += new Vector3(0,scrollValue * scrollScalar,0);
-        ImageToFade1.transform.position += new Vector3(0,scrollValue * scrollScalar,0);
-        TextToFade.transform.position += new Vector3(0,scrollValue * scrollScalar,0);
+        Controlls.transform.position += new Vector3(0,-(scrollValue * scrollScalar),0);
+
+        if(Timeline > 0.11f && Text1.color.a < 1.0f)
+        {
+            fadeCount1 += scrollValue*fadeScalar;
+            Text1.color = new Color(Text1.color.r,Text1.color.g,Text1.color.b,fadeCount1);
+        }
+        if(Timeline > 0.22f && Text2.color.a < 1.0f)
+        {
+            fadeCount2 += scrollValue*fadeScalar;
+            Text2.color = new Color(Text2.color.r,Text2.color.g,Text2.color.b,fadeCount2);
+        }
+        if(Timeline > 0.35f && FadeToBlack.color.a < 1.0f)
+        {
+            fadeCount3 += scrollValue*fadeScalar;
+            FadeToBlack.color = new Color(FadeToBlack.color.r,FadeToBlack.color.g,FadeToBlack.color.b,fadeCount3);
+        }
+        else if (FadeToBlack.color.a >= 1.0f)
+        {
+            EndScene();
+        }
+        
     }
 
-    private void fade()
+    private void EndScene()
     {
-        Debug.Log("Fade");
+        ended = true;
+        Debug.Log("END ALL");
     }
 }
